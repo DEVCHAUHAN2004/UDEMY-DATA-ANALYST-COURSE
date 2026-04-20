0e0d03054b43186326296043ce7b84a1cf0124cc
DEV CHAUHAN <166748796+DEVCHAUHAN2004@users.noreply.github.com>
2026-04-20 23:40:09 +0530

    Create python syntax and semantics.py

---
+++ PYTHON/python syntax and semantics.py
@@ -0,0 +1,56 @@
+
+### Syntax and Semantics in Python
+# Video Outline:
+# - Single line Comments and multiline comments 
+# - Definition of Syntax and Semantics
+# - Basic Syntax Rules in Python
+# - Understanding Semantics in Python
+# - Common Syntax Errors and How to Avoid Them
+# - Practical Code Examples
+
+
+# basic
+name = "dev"
+print(name)
+
+
+# indentation 
+age = 32
+if age > 32:
+  print(age)
+print(age)
+
+# line continuation 
+total = 1+2+3+4+5+6+\
+7+8+9+10
+print(total)
+
+## Multiple Statements on a single line
+x = 5;y=10;z= x+y
+print(z)
+
+
+##Understand  Semnatics In Python
+# variable assignment
+age=32 ##age is an integer
+name="Krish" ##name is a string
+print(type(age))
+print(type(name))
+
+
+## Code exmaples of indentation
+if True:
+    print("Correct Indentation")
+    if False:
+        print("This ont print")
+    print("This will print")
+print("Outside the if block")
+
+### Conclusion:
+# Understanding the syntax and semantics of Python
+#  is crucial for writing correct and meaningful
+#  programs. Syntax ensures the code is properly 
+# structured, while semantics ensures the code behaves
+#  as expected. Mastering these concepts will help in 
+# writing efficient and error-free Python code.
+
